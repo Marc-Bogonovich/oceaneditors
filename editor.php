8b5c9e4dac2dde3e825c570ad53871ba02294272
Marc-Bogonovich <Marc.Bogonovich@openwords.com>
2017-03-22 23:46:14 -0400

    Cleaned up editor.php

--- editor.php
+++ editor.php
@@ -75,7 +75,7 @@ $row = mysql_fetch_assoc($sql);
 echo "
   <div id= 'editors' >
      <ul>
-       <li id= 'username'>Username: <b>". $row["username"] ."</b></li>
+
        <li id= 'name'>Name: <b>". $row["name"] ."</b></li>
        <li id= 'turnaround'>Turnaround Time: <b>". $row["turn_aroundtime"] ."</b> days </li>
        <li id= 'field'>Fields: <b>". $row["fields"] ."</b></li>
@@ -108,7 +108,7 @@ echo "
 
 <div id= 'editors' >
 
-<p style="font-size: 24px;"><b>Contact Editor</b> Please use this form to contact this editor.</p><br>
+<p style="font-size: 24px;"><b>Contact Editor</b><br> Please contact this editor with the following email.</p><br>
 
 
 
@@ -117,7 +117,7 @@ echo "
 
 <!--CONTACT FORM CODE. I used the following tutorial:
 http://tangledindesign.com/how-to-create-a-contact-form-using-html5-css3-and-php/ -->
-
+<!--
     <header class="body">
     </header>
 
@@ -135,7 +135,7 @@ http://tangledindesign.com/how-to-create-a-contact-form-using-html5-css3-and-php
       <label>Message</label>
       <textarea name="message" placeholder="your message"></textarea>
 <br>
-<!--Anti-spam line-->
+
 <label>*What is 2+3? (Anti-spam)</label>
 <input name="human" placeholder="Type Here">
 <br>
@@ -146,7 +146,7 @@ http://tangledindesign.com/how-to-create-a-contact-form-using-html5-css3-and-php
     <footer class="body">
     </footer>
     <br><br><br><br><br><br>
-    
+-->    
 <?php
 // connect to the db to collect the email recipient
 
@@ -158,9 +158,9 @@ http://tangledindesign.com/how-to-create-a-contact-form-using-html5-css3-and-php
     $from = 'From: OceanEditors_Customer_Contact_Form';
 //Might need to reopen db connection if closed above
     $to = "'".$row["email"][0].$row["email"][1].$row["email"][2].$row["email"][3].$row["email"][4].$row["email"][5].$row["email"][6].$row["email"][7].$row["email"][8].$row["email"][9].$row["email"][10].$row["email"][11].$row["email"][12].$row["email"][13].$row["email"][14].$row["email"][15].$row["email"][16].$row["email"][17].$row["email"][18].$row["email"][19].$row["email"][20].$row["email"][21].$row["email"][22].$row["email"][23]."'";
-    echo var_dump($to);
+//    echo var_dump($to);
     $to = $row['email'];
-    echo var_dump($to);
+//    echo var_dump($to);
 //    $to = (string)$to;
 
 //    $to = "'".$to."'";
@@ -171,23 +171,23 @@ http://tangledindesign.com/how-to-create-a-contact-form-using-html5-css3-and-php
 //    $to = "\"$to\"";
 //The email only triggers a successful "sent" when single quotes are placed on the email.. however, adding these single quotes actually leads to the email not being recieved by the intended email. I am only able to successfully send to a hard coded 'contact@oceaneditors.com' and *not* the very same email collected from the db.
 //    $to2 = 'marc.bogonovich@gmail.com';
-    $to = 'contact@oceaneditors.com';
-    echo var_dump($to2);
+//    $to = 'contact@oceaneditors.com';
+//    echo var_dump($to2);
     $to3 = htmlentities($to);
     $subject = 'Hello';
 //TEST    
-    echo "<br>".$to."<br>".$to2."<br>".$to3."<br>".$from."<br>";
+//    echo "<br>".$to."<br>".$to2."<br>".$to3."<br>".$from."<br>";
 //END TEST    
     //Anti-spam line
     $human = htmlspecialchars($_POST['human']);
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
+echo $to."<br><br>";
 //TEST
 if ($to == $to2){
-echo "<br>equal<br>";
+//echo "<br>equal<br>";
 } else {
-echo "<br>not equal<br>";
+//echo "<br>not equal<br>";
 }
 //END TEST
 
