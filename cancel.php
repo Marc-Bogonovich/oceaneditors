8af6a4fdcb9bea58055776f028d829bf482adb2c
Marc-Bogonovich <Marc.Bogonovich@openwords.com>
2017-03-29 09:43:27 -0400

    Create cancel.php

---
+++ cancel.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TransactionCancel</title>
+	<link href="https://fonts.googleapis.com/css?family=Nunito|Raleway|Lora|Merriweather|Libre+Baskerville|Barrio|Quicksand|Pacifico|Alegreya" rel="stylesheet">
+	<link rel="SHORTCUT ICON" href="OceanEditorsLogo_WhiteOnBlue.png">
+<style>
+
+#message {
+font-family: 'Merriweather', serif;
+font-size:14px;
+color: #23505e;
+}
+
+</style>
+
+<body>
+
+<p id="message" >Your transaction was cancelled.</p>
+	<br>
+<!--Here we should try to return the client to the same editor page rather than the OceanEditors main site-->
+	<br>
+<p id="message" >Return to <a href="http://www.oceaneditors.com/">Ocean&middot;Editors</a></p>
+
+</body>
