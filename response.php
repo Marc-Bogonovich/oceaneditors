dc0cd2ff2374929df935426577de71f54792b58c
Marc-Bogonovich <Marc.Bogonovich@openwords.com>
2017-03-29 09:21:02 -0400

    Create response.php

---
+++ response.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TransactionResponse</title>
+	<link href="https://fonts.googleapis.com/css?family=Nunito|Raleway|Lora|Merriweather|Libre+Baskerville|Barrio|Quicksand|Pacifico|Alegreya" rel="stylesheet">
+	<link rel="SHORTCUT ICON" href="OceanEditorsLogo_WhiteOnBlue.png">
+<style>
+
+#message {
+font-family: 'Merriweather', serif;
+font-size:14px;
+color: #23505e;
+}
+
+</style>
+
+<body>
+
+<p id="message" >Your transaction was successful. Thank you for your business.</p>
+<p id="message" >Please share our service with a friend or colleague!</p>
+
+<!--This is a good place to provide a share link to friends-->
+
+<p id="message" >Return to <a href="http://www.oceaneditors.com/">Ocean$middot;Editors</a></p>
+
+</body>
