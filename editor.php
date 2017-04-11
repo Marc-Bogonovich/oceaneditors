2a0b77ce63ef8baabc1fc88be32beea6726a7f81
Marc-Bogonovich <Marc.Bogonovich@openwords.com>
2017-04-11 17:35:59 -0500

    CONTACT EDITOR DIVISION placed as 1st division

--- editor.php
+++ editor.php
@@ -68,10 +68,21 @@ $row = mysql_fetch_assoc($sql);
 //echo "<br><br>" . $row["username"] . "<br>";
 //END TEST
 
+//CONTACT EDITOR DIVISION
+$to = $row['email'];
+echo "<!--CONTACT EDITOR DIVISION-->
+<div id= 'editors' >
+
+<p style=\"font-size: 24px;\"><b>Contact Editor</b><br> Please contact this editor with the following email.</p><br>
+
+".$to."<br><br>
+</div>
+<!--END CONTACT EDITOR DIVISION-->
+";
+//END CONTACT EDITOR DIVISION
 
 
-
-
+//EDITOR INFORMATION DIVISION
 echo "
   <div id= 'editors' >
      <ul>
@@ -79,15 +90,18 @@ echo "
        <li id= 'name'><b>". $row["name"] ."</b></li>
        <li id= 'price'><b>". $row["price"] ."</b></li>
        <br>
+       <li id= 'turnaround'>Turnaround Time: <b>". $row["turn_aroundtime"] ."</b> days </li>
+       <br>
        <li id= 'field'>Fields: <b>". $row["fields"] ."</b></li>
        <!-- <li id= 'lastlogin'>Last Login Time: 1 day</li> -->
-       <li id= 'turnaround'>Turnaround Time: <b>". $row["turn_aroundtime"] ."</b> days </li>
+       <br>
        <li id= 'description'>Description: <b>". $row["description"] ."</b></li>
      </ul>
      &nbsp
   </div>
 
 ";
+//END EDITOR INFORMATION DIVISION
 
 ?>
 
@@ -105,7 +119,7 @@ echo "
 
 
 
-
+<!-- ORIGINAL CONTACT EDITOR DIVISION
 <div id= 'editors' >
 
 <p style="font-size: 24px;"><b>Contact Editor</b><br> Please contact this editor with the following email.</p><br>
@@ -148,73 +162,73 @@ http://tangledindesign.com/how-to-create-a-contact-form-using-html5-css3-and-php
     <br><br><br><br><br><br>
 -->    
 <?php
-// connect to the db to collect the email recipient
-
-
-//I added the htmlspecialchars() function
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
-    $from = 'From: OceanEditors_Customer_Contact_Form';
-//Might need to reopen db connection if closed above
-    $to = "'".$row["email"][0].$row["email"][1].$row["email"][2].$row["email"][3].$row["email"][4].$row["email"][5].$row["email"][6].$row["email"][7].$row["email"][8].$row["email"][9].$row["email"][10].$row["email"][11].$row["email"][12].$row["email"][13].$row["email"][14].$row["email"][15].$row["email"][16].$row["email"][17].$row["email"][18].$row["email"][19].$row["email"][20].$row["email"][21].$row["email"][22].$row["email"][23]."'";
-//    echo var_dump($to);
-    $to = $row['email'];
-//    echo var_dump($to);
-//    $to = (string)$to;
-
-//    $to = "'".$to."'";
-//    $to = substr($to, 1);
-//    $to = substr($to, 0, -1);
-//    $to = $to.",".'contact@oceaneditors.com';
-//    $\_POST['to'] = strval($to);
-//    $to = "\"$to\"";
-//The email only triggers a successful "sent" when single quotes are placed on the email.. however, adding these single quotes actually leads to the email not being recieved by the intended email. I am only able to successfully send to a hard coded 'contact@oceaneditors.com' and *not* the very same email collected from the db.
-//    $to2 = 'marc.bogonovich@gmail.com';
-//    $to = 'contact@oceaneditors.com';
-//    echo var_dump($to2);
-    $to3 = htmlentities($to);
-    $subject = 'Hello';
-//TEST    
-//    echo "<br>".$to."<br>".$to2."<br>".$to3."<br>".$from."<br>";
-//END TEST    
-    //Anti-spam line
-    $human = htmlspecialchars($_POST['human']);
-
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-echo $to."<br><br>";
-//TEST
-if ($to == $to2){
-//echo "<br>equal<br>";
-} else {
-//echo "<br>not equal<br>";
-}
-//END TEST
-
-//Didn't help
-//mysql_close()
+//   // connect to the db to collect the email recipient
+//   
+//   
+//   //I added the htmlspecialchars() function
+//       $name = htmlspecialchars($_POST['name']);
+//       $email = htmlspecialchars($_POST['email']);
+//       $message = htmlspecialchars($_POST['message']);
+//       $from = 'From: OceanEditors_Customer_Contact_Form';
+//   //Might need to reopen db connection if closed above
+//       $to = "'".$row["email"][0].$row["email"][1].$row["email"][2].$row["email"][3].$row["email"][4].$row["email"][5].$row["email"][6].$row["email"][7].$row["email"][8].$row["email"][9].$row["email"][10].$row["email"][11].$row["email"][12].$row["email"][13].$row["email"][14].$row["email"][15].$row["email"][16].$row["email"][17].$row["email"][18].$row["email"][19].$row["email"][20].$row["email"][21].$row["email"][22].$row["email"][23]."'";
+//   //    echo var_dump($to);
+//       $to = $row['email'];
+//   //    echo var_dump($to);
+//   //    $to = (string)$to;
+//   
+//   //    $to = "'".$to."'";
+//   //    $to = substr($to, 1);
+//   //    $to = substr($to, 0, -1);
+//   //    $to = $to.",".'contact@oceaneditors.com';
+//   //    $\_POST['to'] = strval($to);
+//   //    $to = "\"$to\"";
+//   //The email only triggers a successful "sent" when single quotes are placed on the email.. however, adding these single quotes actually leads to the email not being recieved by the intended email. I am only able to successfully send to a hard coded 'contact@oceaneditors.com' and *not* the very same email collected from the db.
+//   //    $to2 = 'marc.bogonovich@gmail.com';
+//   //    $to = 'contact@oceaneditors.com';
+//   //    echo var_dump($to2);
+//       $to3 = htmlentities($to);
+//       $subject = 'Hello';
+//   //TEST    
+//   //    echo "<br>".$to."<br>".$to2."<br>".$to3."<br>".$from."<br>";
+//   //END TEST    
+//       //Anti-spam line
+//       $human = htmlspecialchars($_POST['human']);
+//   
+//       $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+//   echo $to."<br><br>";
+//   //TEST
+//   if ($to == $to2){
+//   //echo "<br>equal<br>";
+//   } else {
+//   //echo "<br>not equal<br>";
+//   }
+//   //END TEST
+//   
+//   //Didn't help
+//   //mysql_close()
 ?>
 
 <?php
-/* Anti-spam: && $human == '5' */
-if ($_POST['submit'] && $human == '5') {
-
-mail($to, $subject, $body, $from);
-
-    if (mail ($to, $subject, $body, $from)) {
-        echo '<p>Your message has been sent!</p>';
-    } else {
-        echo '<p>Something went wrong, go back and try again!</p>';
-    }
-} /* Anti-spam else if element */ else if ($_POST['submit'] && $human != '5') {
-    echo '<p>You answered the anti-spam question incorrectly!</p>';
-}
+//   /* Anti-spam: && $human == '5' */
+//   if ($_POST['submit'] && $human == '5') {
+//   
+//   mail($to, $subject, $body, $from);
+//   
+//       if (mail ($to, $subject, $body, $from)) {
+//           echo '<p>Your message has been sent!</p>';
+//       } else {
+//           echo '<p>Something went wrong, go back and try again!</p>';
+//       }
+//   } /* Anti-spam else if element */ else if ($_POST['submit'] && $human != '5') {
+//       echo '<p>You answered the anti-spam question incorrectly!</p>';
+//   }
 ?>
 
 
 <!--END CONTACT FORM CODE-->
-  </div>
-  
+<!--  </div>
+  END ORIGINAL CONTACT DIVISION DIVISION -->
   
   
 <!--PAYMENT DIVISION-->  
